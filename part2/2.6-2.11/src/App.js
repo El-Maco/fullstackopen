7b62c386442f7da3bf1ab3313712a0114d195b94
Marcus Myllyviita <marcus.myllyviita@gmail.com>
2021-10-21 15:44:37 +0300

    Added Part2.11

--- part2/2.6-2.11/src/App.js
+++ part2/2.6-2.11/src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm.js'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567', id: 1},
-    { name: 'Mikko Makinen', number: '040-9876547', id: 2},
-    { name: 'Ada Lovelace', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
+
+  // Fetch person data from database
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  },  [])
+  console.log('rendered', persons.length, 'persons')
 
   const addPerson = (event) => {
     event.preventDefault()
