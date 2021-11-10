6fd5e23ec04ccc98c3ddb9a2f127b6cf47c9ec0a
Marcus Myllyviita <marcus.myllyviita@gmail.com>
2021-11-10 17:12:47 +0200

    2.6-2.11 - added Person component

--- part2/2.6-2.11/src/components/Persons.js
+++ part2/2.6-2.11/src/components/Persons.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Person from './Person'
 
 const Persons = ({ persons }) => {
   return (
-    persons.map(person => <li key={person.id}>{person.name} {person.number}</li>)
+    persons.map(person => <Person person={person} />)
   )
 }
 
