3f454e14b0c4148a05f85397e451ebc4a61a5467
Marcus Myllyviita <marcus.myllyviita@gmail.com>
2022-05-27 16:43:40 +0300

    Part4.8 Completed

---
+++ part4/blog-list-app/tests/blogs_api.test.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blogs')
+const helper = require('./test_helper')
+const api = supertest(app)
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('blog tests', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific title is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain(
+      'React patterns'
+    )
+  })
+})
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
