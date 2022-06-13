95c7440be9d3e7a2e975d858251abd4b6c8522bc
Marcus Myllyviita <marcus.myllyviita@gmail.com>
2022-06-13 21:04:23 +0300

    5.7 Completed

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title}, {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {  
+  const [visible, setVisible] = useState(false)
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <p>{blog.title}, {blog.author} <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button></p>
+      </div>  
+    <div style={showWhenVisible}>
+      <ul>
+        <li>{blog.url}</li>
+        <li>{blog.likes}</li>
+        <li>{blog.author}</li>
+      </ul>
+    </div>
+    </div>
+  )
+}
 
 export default Blog
